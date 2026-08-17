8a26c9b22bd07678a9bf28ba73d862274bf367d2
andevdrade <jvitorrr5.0@gmail.com>
2026-08-17 08:56:19 -0300

    criptografia

--- obj/Release/net10.0/ApiLibertadoresHAS.AssemblyInfo.cs
+++ obj/Release/net10.0/ApiLibertadoresHAS.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiLibertadoresHAS")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44a8bc278aece204c0cab17ea2031dbc74a6defa")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiLibertadoresHAS")]
